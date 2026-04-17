ba95cc8b12451c0bc2039eab0b028e3d5a0d6568
Jose Esteban Ruiz Vertel <jruiz@q10.com>
2026-04-16 20:19:11 -0500

    Corrección de errores mostrados en el cloud, aun hay uno

--- cypress/e2e/action/type.action.ts
+++ cypress/e2e/action/type.action.ts
@@ -1,6 +1,6 @@
 export class TypeAction {
     
     static typeIn (elemento: string, texto: string) {
-        cy.get(elemento).clear().type(texto, {delay: 20})
+        cy.get(elemento).clear().type(texto, {delay: 60})
     }
 }
